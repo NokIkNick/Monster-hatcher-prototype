204f8257e8e744b81390d831129321c72b40a420
Nicklas Winther <Nicklas2002@hotmail.com>
2024-08-01 03:25:41 +0200

    Setting up egg stats

--- game/scenes/egg.gd
+++ game/scenes/egg.gd
@@ -4,10 +4,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var rng = RandomNumberGenerator.new()
+	var random_number= rng.randi_range(1, EggStats.Rarity.size() -1)
 	if (stats):
-		stats.health = 10
-		stats.rarity = EggStats.Rarity.COMMON
-		print(stats.rarity) 
+		stats.rarity = EggStats.Rarity.values()[random_number]
+		stats.health = 10 * random_number
+		print(stats.rarity)
+		print(stats.health) 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
